f5dcfc8e37edf3c2fd4bbd6ad8ebe61a7744c540
Dimitar Mitev <mitev.dimitar@gmail.com>
2023-04-18 08:18:27 +0300

    create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material';
+
+function Home() {
+  return (
+    <Grid
+      sx={{
+        background: 'lightgrey',
+        height: '100vh',
+      }}
+    >
+      Stefi Miteva
+    </Grid>
+  );
+}
+
+export default Home;
