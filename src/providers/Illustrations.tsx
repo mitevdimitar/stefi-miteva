93110aff3cbb8145b14b8e00f28fd3ee7268ec55
Dimitar Mitev <mitev.dimitar@gmail.com>
2024-06-21 08:39:26 +0300

    fix reducers imports

--- src/providers/Illustrations.tsx
+++ src/providers/Illustrations.tsx
@@ -4,7 +4,7 @@ import {
   IllustrationsState,
   illustrationsReducer,
   initalIllustrationsState,
-} from '../reducers/Illustrations';
+} from '../reducers/illustrations';
 import { getStorageIllustrations } from '../services/illustrations';
 
 interface IllustrationsContext {
