cc80f604a92a5376e5b47a0ddbfbdc2e4725c317
Dimitar Mitev <mitev.dimitar@gmail.com>
2024-12-03 08:57:59 +0200

    Login form with firebase auth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,54 @@
+import { Stack, Box, TextField, Button } from '@mui/material';
 import Layout from '../components/Layout';
+import { loginFirebase } from '../services/auth';
 
 function Login() {
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email') as string;
+    const password = data.get('password') as string;
+    console.log({
+      email,
+      password,
+    });
+    await loginFirebase(email, password);
+  };
+
   return (
     <Layout>
-      <>Login</>
+      <Stack>
+        <Box component="form" onSubmit={handleLogin} noValidate>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Log In
+          </Button>
+        </Box>
+      </Stack>
     </Layout>
   );
 }
