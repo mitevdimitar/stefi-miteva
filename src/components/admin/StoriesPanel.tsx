895eacbb482926364b8b705256523d1e877503ac
Dimitar Mitev <mitev.dimitar@gmail.com>
2024-12-04 09:30:14 +0200

    Add auth observer to auth provider

--- src/components/admin/StoriesPanel.tsx
+++ src/components/admin/StoriesPanel.tsx
@@ -3,25 +3,18 @@ import Layout from '../Layout';
 import { useContext } from 'react';
 import { StoriesStore } from '../../providers/stories';
 import StoryRow from './StoryRow';
-import { useAuth } from '../../providers/auth';
 
 function StoriesPanel() {
   const {
     state: { stories },
   } = useContext(StoriesStore);
-  const { isLogged } = useAuth();
-  console.log({ isLogged });
 
   return (
     <Layout>
       <Stack gap={1} alignItems={'start'} width={'100%'}>
-        {isLogged ? (
-          <div>Not logged in</div>
-        ) : (
-          stories?.map((story, ind) => {
-            return <StoryRow key={ind} story={story} />;
-          })
-        )}
+        {stories?.map((story, ind) => {
+          return <StoryRow key={ind} story={story} />;
+        })}
       </Stack>
     </Layout>
   );
--- src/providers/auth.tsx
+++ src/providers/auth.tsx
@@ -1,14 +1,24 @@
-import { createContext, useContext, useState } from 'react';
+import { User } from 'firebase/auth';
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+import { observeAuthState } from '../services/auth';
 
-interface AuthContext {
+interface IAuthContext {
+  authChecked: boolean;
   isLogged: boolean;
+  user: User | null;
 }
 
-interface AuthProviderProps {
-  children: React.ReactNode;
-}
-
-export const AuthContext = createContext<AuthContext>({ isLogged: false });
+export const AuthContext = createContext<IAuthContext>({
+  authChecked: false,
+  isLogged: false,
+  user: null,
+});
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
@@ -22,8 +32,27 @@ export const useAuth = () => {
 
 const { Provider } = AuthContext;
 
-export function AuthProvider({ children }: AuthProviderProps) {
-  const [isLogged, _setIsLogged] = useState(false);
+export function AuthProvider({ children }: PropsWithChildren) {
+  const [authChecked, setAuthChecked] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
 
-  return <Provider value={{ isLogged }}>{children}</Provider>;
+  useEffect(() => {
+    const unsubscribe = observeAuthState((user) => {
+      if (user) {
+        setIsLogged(true);
+        setUser(user);
+      } else {
+        setIsLogged(false);
+        setUser(null);
+      }
+      setAuthChecked(true);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <Provider value={{ isLogged, user, authChecked }}>{children}</Provider>
+  );
 }
