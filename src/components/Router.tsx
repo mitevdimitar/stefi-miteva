61704af8ffd9acf0a95ae693f62b9afde12fa94d
Dimitar Mitev <mitev.dimitar@gmail.com>
2024-12-18 09:02:43 +0200

    Introduce lazy page loading in router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,55 +1,62 @@
+import { Suspense, lazy } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Outlet,
 } from 'react-router-dom';
-import Home from '../pages/Home';
-import Illustrations from '../pages/Illustrations';
-import Books from '../pages/Books';
-import Feedback from '../pages/Feedback';
-import Contacts from '../pages/Contacts';
-import About from '../pages/About';
-import Story from '../pages/Story';
-import Stories from '../pages/Stories';
-import Error from '../pages/Error';
-import StoriesPanel from './admin/StoriesPanel';
-import EditStory from '../pages/admin/EditStory';
-import CreateStory from '../pages/admin/CreateStory';
-import Login from '../pages/Login';
+const Home = lazy(() => import('../pages/Home'));
+const Books = lazy(() => import('../pages/Books'));
+const Feedback = lazy(() => import('../pages/Feedback'));
+const Contacts = lazy(() => import('../pages/Contacts'));
+const About = lazy(() => import('../pages/About'));
+const Story = lazy(() => import('../pages/Story'));
+const Stories = lazy(() => import('../pages/Stories'));
+const Illustrations = lazy(
+  () =>
+    import(/* webpackChunkName: "[illustrations]" */ '../pages/Illustrations')
+);
+const Error = lazy(() => import('../pages/Error'));
+const Login = lazy(() => import('../pages/Login'));
+const StoriesPanel = lazy(() => import('./admin/StoriesPanel'));
+const EditStory = lazy(() => import('../pages/admin/EditStory'));
+const CreateStory = lazy(() => import('../pages/admin/CreateStory'));
 import ProtectedRoute from './ProtectedRoute';
 import Layout from './Layout';
+import Loader from './Loader';
 
 export default function AppRouter() {
   return (
     <Router>
       <Layout>
-        <Routes>
-          <Route path="/illustrations" element={<Illustrations />} />
-          <Route path="/books" element={<Books />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/stories" element={<Outlet />}>
-            <Route index element={<Stories />} />
-            <Route path=":slug" element={<Story />} />
-          </Route>
-          <Route
-            path="/stories-panel"
-            element={
-              <ProtectedRoute>
-                <Outlet />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<StoriesPanel />} />
-            <Route path="create" element={<CreateStory />} />
-            <Route path="edit/:slug" element={<EditStory />} />
-          </Route>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/illustrations" element={<Illustrations />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/stories" element={<Outlet />}>
+              <Route index element={<Stories />} />
+              <Route path=":slug" element={<Story />} />
+            </Route>
+            <Route
+              path="/stories-panel"
+              element={
+                <ProtectedRoute>
+                  <Outlet />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<StoriesPanel />} />
+              <Route path="create" element={<CreateStory />} />
+              <Route path="edit/:slug" element={<EditStory />} />
+            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </Router>
   );
